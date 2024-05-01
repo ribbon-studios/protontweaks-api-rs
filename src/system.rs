a4ceccdc3e8680c7f1528e1c1cffe8d75d418634
Cecilia Sanare <ceci@sanare.dev>
2024-05-01 12:27:03 -0500

    fix: added unknown gpu type

--- src/system.rs
+++ src/system.rs
@@ -24,9 +24,11 @@ pub struct SystemTweaks {
     pub settings: TweakSettings,
 }
 
+#[derive(PartialEq, Debug)]
 pub enum GPU {
     AMD,
     NVIDIA,
+    UNKNOWN,
 }
 
 impl GPU {
@@ -34,13 +36,14 @@ impl GPU {
         match self {
             GPU::AMD => "amd",
             GPU::NVIDIA => "nvidia",
+            GPU::UNKNOWN => "unknown",
         }
     }
 }
 
 pub struct SystemInfo {
     pub driver: String,
-    pub driver_type: Option<GPU>,
+    pub driver_type: GPU,
 }
 
 pub async fn get_system_info() -> Option<SystemInfo> {
@@ -53,32 +56,38 @@ pub async fn get_system_info() -> Option<SystemInfo> {
     Some(SystemInfo {
         driver: adapter.get_info().driver,
         driver_type: match adapter.get_info().driver.as_ref() {
-            "NVIDIA" => Some(GPU::NVIDIA),
+            "NVIDIA" => GPU::NVIDIA,
             // TODO: Need someone with an AMD GPU to verify this is correct
-            "AMD" => Some(GPU::AMD),
-            _ => None,
+            "AMD" => GPU::AMD,
+            _ => GPU::UNKNOWN,
         },
     })
 }
 
 impl GpuDriver {
-    async fn get_gpu_info(&self) -> Option<GPU> {
-        let system_info = get_system_info().await?;
-
-        system_info.driver_type
+    async fn get_gpu_info(&self) -> GPU {
+        if let Some(system_info) = get_system_info().await {
+            system_info.driver_type
+        } else {
+            GPU::UNKNOWN
+        }
     }
 
     pub async fn get_tweaks(&self) -> &Option<SystemTweaks> {
-        self.get_gpu_info().await.map_or(&None, |gpu| {
-            info!("Detected gpu as '{}'", gpu.as_str());
-            self.get_tweaks_for_gpu(gpu)
-        })
+        match self.get_gpu_info().await {
+            GPU::UNKNOWN => &None,
+            gpu => {
+                info!("Detected gpu as '{}'", gpu.as_str());
+                self.get_tweaks_for_gpu(gpu)
+            }
+        }
     }
 
     pub fn get_tweaks_for_gpu(&self, gpu: GPU) -> &Option<SystemTweaks> {
         match gpu {
             GPU::AMD => &self.amd,
             GPU::NVIDIA => &self.nvidia,
+            GPU::UNKNOWN => &None,
         }
     }
 }
